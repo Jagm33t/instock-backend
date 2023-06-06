5a0e499e7e3633bd0ccd20983b97d93649e15f8b
Marcelo <mjfcamspo@gmail.com>
2023-06-06 16:53:35 -0700

    add basic post route and responde on add new inventory item

--- controllers/inventoriesController.js
+++ controllers/inventoriesController.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const knex = require("knex")(require("../knexfile"));
 require("dotenv").config(); // load variables from .env file
 const PORT = process.env.PORT || 8080; // Set server port from .env file
 const SERVER_URL = process.env.SERVER_URL;
 
-// GET functions
-function getWarehouses(req, res) {
-  res.status(200).send("get working");
+// POST functions
+function postInventoryItem(req, res) {
+  res.status(200).send("POST new inventory Item");
 }
 
 module.exports = {
-  getWarehouses,
+  postInventoryItem,
 };
