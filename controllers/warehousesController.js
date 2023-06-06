337c0eba19903087ebf853c8b0ad2145c1b69c58
Isaba Nusrat <isabansrt@gmail.com>
2023-06-06 16:56:02 -0700

    get API warehouse data with Knex

--- controllers/warehousesController.js
+++ controllers/warehousesController.js
@@ -1,10 +1,29 @@
+const knex = require("knex")(require("../knexfile"));
+
 require("dotenv").config(); // load variables from .env file
 const PORT = process.env.PORT || 8080; // Set server port from .env file
 const SERVER_URL = process.env.SERVER_URL;
 
 // GET functions
 function getWarehouses(req, res) {
-  res.status(200).send("get working");
+  knex("warehouses")
+    .select(
+      "id",
+      "warehouse_name",
+      "address",
+      "city",
+      "country",
+      "contact_name",
+      "contact_position",
+      "contact_phone",
+      "contact_email"
+    )
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving Warehouses: ${err}`)
+    );
 }
 
 module.exports = {
