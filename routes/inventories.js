e4af14f8aa15c96727b71ba0f839d58f5cbb5ec0
Karine Kotsubo <kkotsubo@proton.me>
2023-06-07 10:05:47 -0700

    start join name_warehouses id to inventoryList

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
 const inventoriesController = require("../controllers/inventoriesController");
 
-const { postInventoryItem } = inventoriesController;
+const { postInventoryItem, getInventoryItems } = inventoriesController;
 
 require("dotenv").config(); // load variables from .env file
 const PORT = process.env.PORT || 8080; // Set server port from .env file
 const SERVER_URL = process.env.SERVER_URL;
+
+/*
+ * GET Inventory Item
+ */
+router.get("/", getInventoryItems);
+
+/*
+ * GET Inventory Item - Join wareshouse data name
+ */
+router.route("/:id/warehouse_name").get((req, res) => {
+  knex("warehouse_name").join("warehouse_name");
+});
 
 /*
  * POST Inventory Item
